a06e773eb70d531be1bd5f63fad25ba3a2c81ec6
KnechtRootrechtCH <github@atheos.cc>
2021-05-28 15:44:36 +0200

    firebase config

---
+++ src/plugins/fireConfig.js
@@ -0,0 +1,11 @@
+// check firebase console for correct values
+const fireConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+export { fireConfig };
